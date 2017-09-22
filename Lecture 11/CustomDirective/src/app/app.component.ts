f2de766ce6bcaa1c33f0466a6c638d2fc9d6ad15
unknown <yaman_upadhayay@yahoo.com>
2017-09-22 11:12:00 -0500

    Event Handler Directive

--- Lecture 11/CustomDirective/src/app/app.component.ts
+++ Lecture 11/CustomDirective/src/app/app.component.ts
@@ -2,17 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<ul>
-                <li *ngFor="let x of inputs; let num = index" appUpper>{{x}}</li>
-                
+  template: `<ul>            
+                  <li *ngFor="let x of inputs; let num = index" appUpper>{{x}}</li>
+              
               </ul>
-              <p [appMyvisibility]=true>This should hide...!!</p>
+              <p [appMyvisibility]=false>This should hide...!!! (Change directive to true to hide)</p>
+              <p appMycolor>Changes color in click</p>
               `,
   //templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   public inputs: string[];
+
   constructor(){
     this.inputs = ['Yaman','George','Mike'];
   }
--- Lecture 11/CustomDirective/src/app/mycolor.directive.ts
+++ Lecture 11/CustomDirective/src/app/mycolor.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer, HostListener, EventEmitter, Output } from '@angular/core';
 
 @Directive({
   selector: '[appMycolor]'
 })
+
 export class MycolorDirective {
 
-  constructor() { }
+  public inputs: string[] = ["Red", "Blue", "Green", "Yellow", "Purple", "Orange"];
+
+  @Output()
+  emittedValue = new EventEmitter();
+
+  constructor(public el: ElementRef, public renderer: Renderer) { }
+
+  @HostListener('click', ['$event'])
+    onClick(event : MouseEvent) : void {  
+      const randomIndex = Math.floor(Math.random() * this.inputs.length); 
+          this.renderer.setElementStyle(this.el.nativeElement,'color',this.inputs[randomIndex]);
+          this.emittedValue.emit(this.inputs[randomIndex]);
+    }
 
 }
